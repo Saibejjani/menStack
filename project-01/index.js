c110aaa16cbf0754e55c4b4fb509777d81d5c99b
Saibejjani <64737059+Saibejjani@users.noreply.github.com>
2023-07-01 17:05:28 +0530

    api route created to fetch users

--- project-01/index.js
+++ project-01/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const users = require('./MOCK_DATA.json');
 
 const app = express();
 const port = 8000;
+
+//Routes
+
+app.get('/api/users', (req, res) => {
+  res.json(users);
+});
 
 app.listen(port, () => {
   console.log(`server started at http:localhost:${port}`);
